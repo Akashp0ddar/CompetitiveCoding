2840321c43e6fcae6b219ebb9fdc030c703762ae
akashp0ddar <akashpoddar40372gmail.com>
2023-10-10 16:40:54 +0530

    3 more problem solved

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        Solutions solutions = new Solutions();
-        int[] nums = {1,3,5,6};
-        System.out.println(solutions.searchInsert(nums,5));
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<Integer>();
+        List<Long> list2 = new ArrayList<>();
+        list.add(-4);
+        list.add(3);
+        list.add(-9);
+        list.add(0);
+        list.add(4);
+        list.add(1);
+        list2.add(1000000001L);
+        list2.add(1000000002L);
+        list2.add(1000000003L);
+        list2.add(1000000004L);
+        list2.add(1000000005L);
+        System.out.println();
+        Solutions.plusMinus(list);
     }
 }
--- src/Solutions.java
+++ src/Solutions.java
@@ -1,5 +1,105 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solutions {
 
+
+    /**
+     * <a href="https://www.hackerrank.com/challenges/solve-me-first/problem">...</a>
+     */
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * <a href="https://www.hackerrank.com/challenges/simple-array-sum/problem">...</a>
+     */
+    public static int simpleArraySum(List<Integer> ar) {
+        int sum = 0;
+        // Write your code here
+        for (int i : ar) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    // 30 Day Logic building
+
+    /**
+     * <a href="https://www.hackerrank.com/challenges/compare-the-triplets/problem">...</a>
+     */
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        List<Integer> score = new ArrayList<>(2);
+        int aliceScore = 0;
+        int bobScore = 0;
+        for (int i = 0; i < a.size(); i++) {
+            if (a.get(i) < b.get(i)) {
+                bobScore++;
+            } else if (a.get(i) > b.get(i)) {
+                aliceScore++;
+            }
+        }
+        score.add(aliceScore);
+        score.add(bobScore);
+        return score;
+    }
+
+    /**
+     * <a href="https://www.hackerrank.com/challenges/a-very-big-sum/problem">...</a>
+     */
+    public static long aVeryBigSum(List<Long> ar) {
+        // Write your code here
+        Long i = 0L;
+        for (Long g : ar) {
+            i += g;
+        }
+        return i;
+    }
+
+    /**
+     * <a href="https://www.hackerrank.com/challenges/mini-max-sum/problem">...</a>
+     */
+    public static void miniMaxSum(List<Integer> arr) {
+        Integer min = Integer.MAX_VALUE;
+        Integer max = Integer.MIN_VALUE;
+        Long sum = 0L;
+        for (int i = 0; i < arr.size(); i++) {
+            Integer num = arr.get(i);
+            sum += num;
+            if (max < num)
+                max = num;
+            if (min > num)
+                min = num;
+        }
+        System.out.println((sum - max) + " " + (sum - min));
+
+    }
+
+    /**
+     * <a href="https://www.hackerrank.com/challenges/plus-minus/problem">
+     * </a>
+     */
+    public static void plusMinus(List<Integer> arr) {
+        // Write your code here
+        float positiveValue = 0f;
+        float negativeValue = 0f;
+        float zero = 0f;
+        for (Integer integer : arr) {
+            if (integer > 0) {
+                positiveValue++;
+            } else if (integer < 0) {
+                negativeValue++;
+            } else {
+                zero++;
+            }
+        }
+
+        String positiveOutput = String.format("%.6f", positiveValue / arr.size());
+        String negativeOutput = String.format("%.6f", negativeValue / arr.size());
+        String zeroOutput = String.format("%.6f", zero / arr.size());
+        System.out.println(positiveOutput + "\n" + negativeOutput + "\n" + zeroOutput);
+
+    }
 
     /**
      * <a href="https://leetcode.com/problems/remove-element/">...</a>
@@ -20,12 +120,12 @@ public class Solutions {
      */
     public int searchInsert(int[] nums, int target) {
         int start = 0;
-        int end = nums.length-1;
-        while (start<=end){
-            int mid = start+(end-start)/2;
-            if (nums[mid]==target) return mid;
-            else if (nums[mid]>target) end = mid-1;
-            else start = mid+1;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) end = mid - 1;
+            else start = mid + 1;
         }
         return start;
     }
